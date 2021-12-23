dd7b8986bebc781253e6c53bf3ffad354d64a256
unknown <hellobachchi@gmail.com>
2021-12-24 00:00:44 +0530

    first commit

--- my_bot.py
+++ my_bot.py
@@ -11,25 +11,25 @@ user_tasks = []
 updater = Updater(TOKEN)
 
 
-def purga():
-    while True:
-        sleep(1)
-        if user_tasks:
-            try:
-                torlist = list_movies()['result']['pageData']
-                for tit in torlist:
-                    for task in user_tasks:
-                        if tit['torrentDTO']['torrentHash'] in task[2]:
-                            torname = tit['torrentDTO']['torrentName']
+# def purga():
+#     while True:
+#         sleep(1)
+#         if user_tasks:
+#             try:
+#                 torlist = list_movies()['result']['pageData']
+#                 for tit in torlist:
+#                     for task in user_tasks:
+#                         if tit['torrentDTO']['torrentHash'] in task[2]:
+#                             torname = tit['torrentDTO']['torrentName']
 
-                            if torname != None and tit['torrentDTO']['progress'] == 100.0:
-                                torhash = tit['torrentDTO']['torrentHash']
-                                dlink = gen_dow_link(torhash)
-                                updater.bot.send_message(
-                                    chat_id=task[0], text=torname+"\n\n"+dlink)
-                                user_tasks.remove(task)
-            except:
-                pass
+#                             if torname != None and tit['torrentDTO']['progress'] == 100.0:
+#                                 torhash = tit['torrentDTO']['torrentHash']
+#                                 dlink = gen_dow_link(torhash)
+#                                 updater.bot.send_message(
+#                                     chat_id=task[0], text=torname+"\n\n"+dlink)
+#                                 user_tasks.remove(task)
+#             except:
+#                 pass
 
 
 def start(update: Update, context: CallbackContext):
@@ -74,14 +74,14 @@ def downloader(update, context):
 
 
 
-updater.dispatcher.run_async(purga)
+# updater.dispatcher.run_async(purga)
 updater.dispatcher.add_handler(MessageHandler(Filters.document, downloader))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('test', testing))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, send_mag)) 
 
-updater.start_webhook(listen='0.0.0.0', port=443, url_path=TOKEN,)
-updater.bot.setWebhook('https://torbaby.herokuapp.com/' + TOKEN)
+updater.start_webhook(listen='0.0.0.0', port=443, url_path=TOKEN,
+                      webhook_url='https://torbaby.herokuapp.com/' + TOKEN)
 # updater.start_polling()
 updater.idle()
